98a98e3d4d69ed5bcf678bfa04fa61afc222beb2
Emmanuel Garzon <eggarzon@gmail.com>
2022-04-01 10:25:14 -0700

    updating cart.js

--- cart.js
+++ cart.js
@@ -7,7 +7,6 @@
     creating customer objects.  
 */
 
-
 //////////////////PROBLEM 1////////////////////
 /*  
     Below is a cart array that has food objects
@@ -19,24 +18,26 @@
 */
 
 const cart = [
-    {
-        name: 'pizza', 
-        price: 9.99
-    }, 
-    {
-        name: 'pasta', 
-        price: 8.99
-    }, 
-    {
-        name: 'salad', 
-        price: 7.99
-    }
-]
+  {
+    name: "pizza",
+    price: 9.99,
+  },
+  {
+    name: "pasta",
+    price: 8.99,
+  },
+  {
+    name: "salad",
+    price: 7.99,
+  },
+];
 
 //CODE HERE
 
-// const summedPrice = cart.reduce(/* CALLBACK HERE */)
-
+const summedPrice = cart.reduce((acc, currVal) => {
+  return acc + currVal.price;
+}, 0);
+console.log(summedPrice);
 
 //////////////////PROBLEM 2////////////////////
 /*  
@@ -55,7 +56,12 @@ const cart = [
 
 //CODE HERE
 
+const calcFinalPrice = (cartTotal, couponValue, tax) => {
+  let totalPrice = cartTotal + cartTotal * tax - couponValue;
+  return totalPrice;
+};
 
+console.log(calcFinalPrice(95, 15, 0.06));
 
 //////////////////PROBLEM 3////////////////////
 /*  
@@ -80,6 +86,12 @@ const cart = [
 /*
     TEXT ANSWER HERE
 
+    NAME: string type, acting a means to identify the customer
+    SHIPPING ADDRESS: string type, to know where any items ordered should be shipped
+    BILLING ADDRESS: string type, its possible that the billing information will differ from the shipping (ie. sending gifts)
+    CREDIT CARD NUMBER: number type, I am not sure if credit card values can be securely stored in objects, but if so, then to save the user the trouble of having to re-enter this information everytime they make an online purchase
+
+
 */
 
 /*
@@ -88,3 +100,20 @@ const cart = [
 */
 
 //CODE HERE
+
+const customers = [
+  {
+    fullName: "Emmanuel Garzon",
+    billingAddress: "123 Main Rd.",
+    shippingAddress: "456 2nd Avenue",
+    creditCardNumber: 123456789,
+  },
+  {
+    fullName: "Hamato Yoshi",
+    billingAddress: "123 Sewer Rd.",
+    shippingAddress: "456 Park Place",
+    creditCardNumber: 235475962,
+  },
+];
+
+console.log(customers);
